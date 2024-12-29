e4b3e024d63807558e96cc6b9e2287d30e21f2ec
Alina <241516@astanait.edu.kz>
2024-12-29 17:09:19 +0500

    Completed assignment

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,151 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+// Base class: Person
+class Person {
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected boolean gender; // true for Male, false for Female
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String genderString = gender ? "Male" : "Female";
+        return "Hi, I am " + name + " " + surname + ", a " + age + "-year-old " + genderString + ".";
+    }
+}
+
+// Derived class: Student
+class Student extends Person {
+    private static int idCounter = 1;
+    private final int studentID;
+    private final List<Integer> grades;
+
+    public Student(String name, String surname, int age, boolean gender) {
+        super(name, surname, age, gender);
+        this.studentID = idCounter++;
+        this.grades = new ArrayList<>();
+    }
+
+    public void addGrade(int grade) {
+        if (grade >= 0 && grade <= 100) {
+            grades.add(grade);
+        } else {
+            throw new IllegalArgumentException("Grade must be between 0 and 100.");
+        }
+    }
+
+    public double calculateGPA() {
+        if (grades.isEmpty()) return 0.0;
+        int total = 0;
+        for (int grade : grades) {
+            total += grade;
+        }
+        return total / (double) grades.size();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " I am a student with ID " + studentID + ".";
+    }
+}
+
+// Derived class: Teacher
+class Teacher extends Person {
+    private String subject;
+    private int yearsOfExperience;
+    private int salary;
+
+    public Teacher(String name, String surname, int age, boolean gender, String subject, int yearsOfExperience, int salary) {
+        super(name, surname, age, gender);
+        this.subject = subject;
+        this.yearsOfExperience = yearsOfExperience;
+        this.salary = salary;
+    }
+
+    public void giveRaise(double percentage) {
+        if (percentage < 0) {
+            throw new IllegalArgumentException("Raise percentage must be positive.");
+        }
+        salary += salary * (percentage / 100);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " I teach " + subject + ".";
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
+
+// School class
+class School {
+    private final List<Person> members;
+
+    public School() {
+        members = new ArrayList<>();
+    }
+
+    public void addMember(Person person) {
+        members.add(person);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Person member : members) {
+            result.append(member.toString()).append("\n");
+        }
+        return result.toString();
+    }
+}
+
+// Main class for testing
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        School school = new School();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Adding students
+        Student harry = new Student("Harry", "Potter", 21, true);
+        harry.addGrade(78); harry.addGrade(89); harry.addGrade(99); harry.addGrade(51);
+
+        Student ron = new Student("Ron", "Weasley", 20, true);
+        ron.addGrade(78); ron.addGrade(89); ron.addGrade(75); ron.addGrade(63); ron.addGrade(82);
+
+        Student hermione = new Student("Hermione", "Granger", 19, false);
+        hermione.addGrade(100); hermione.addGrade(99); hermione.addGrade(95);
+        hermione.addGrade(98); hermione.addGrade(100); hermione.addGrade(96); hermione.addGrade(97);
+
+        // Adding teachers
+        Teacher snape = new Teacher("Severus", "Snape", 51, true, "Math", 7, 800000);
+        Teacher dumbledore = new Teacher("Albus", "Dumbledore", 71, true, "Philosophy", 25, 1500000);
+        Teacher mcgonagall = new Teacher("Minerva", "McGonagall", 62, false, "Sociology", 17, 1100000);
+
+        // Adding members to the school
+        school.addMember(harry);
+        school.addMember(ron);
+        school.addMember(hermione);
+        school.addMember(snape);
+        school.addMember(dumbledore);
+        school.addMember(mcgonagall);
+
+        // Giving raise to teachers with more than 10 years of experience
+        if (dumbledore.getYearsOfExperience() > 10) dumbledore.giveRaise(10);
+        if (mcgonagall.getYearsOfExperience() > 10) mcgonagall.giveRaise(10);
+
+        // Printing all members
+        System.out.println(school);
     }
 }
